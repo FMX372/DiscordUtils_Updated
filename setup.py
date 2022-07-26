42da834e4aa705268a135284b26627689e70bdc4
Komrade FMX <62594965+FMX372@users.noreply.github.com>
2022-07-26 09:33:07 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,10 +7,10 @@ setuptools.setup(
     name="NextcordUtils_Reloaded",
     version="1.3.4",
     author="toxicrecker, modified by Komrade FMX for Nextcord",
-    description="DiscordUtils is a very useful library made to be used with nextcord",
+    description="NextcordUtils is a very useful library made to be used with nextcord",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/FMX372/DiscordUtils_Updated",
+    url="https://github.com/FMX372/NextcordUtils_Reloaded",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
